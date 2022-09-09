07fd175e18808a1256a1174986fbae75a55d1391
Emily Manchego-Stillwell <emanchego85@gmail.com>
2022-09-09 16:38:17 -0600

    Added a circle marker to the map over Los Angeles.

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,14 +2,37 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
-// We create the tile layer that will be the background of our map
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// Add a marker to the map for Los Angeles, California
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+//Changing the marker to a circle
+// let marker = L.circle([34.0522, -118.2437], {
+//     radius:100
+// }).addTo(map);
+
+// Skill Drill 13.4.1
+let marker = L.circleMarker([34.0522, -118.2437], {
+    color: 'black',
+    fillColor: '#f5f395',
+    fillOpacity: 0.20,
+    radius:300,
+}).addTo(map);
+
+// // We create the tile layer that will be the background of our map
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     accessToken: API_KEY
+// });
+
+// Skill Drill 13.4.1
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
 // Then we add our 'graymap' tile layer to the map
-streets.addTo(map);
+dark.addTo(map);
