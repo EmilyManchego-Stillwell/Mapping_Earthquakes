31796d9930b9833a36b404ec28228f6f31ea7154
Emily Manchego-Stillwell <emanchego85@gmail.com>
2022-09-12 11:47:34 -0600

    Modified logic.js file to add multiple layers to a map for different view options.

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,10 +1,14 @@
 // add console.log to check to see if our code is working
 console.log("working");
 
+// ******* Create the map object with setView() method. Used when only using single tile layer for map***********
 // // Create the map object with a center at San Francisco Airport and zoom level 10.
 // let map = L.map('mapid').setView([37.6213, -122.3790], 10);
-// Create the map object with center at center of the Earth and zoom level 2
-let map = L.map('mapid').setView([30, 30], 2);
+// // Create the map object with center at center of the Earth and zoom level 2
+// let map = L.map('mapid').setView([30, 30], 2);
+
+// ******* Create the map object with alternative method, which is used when using multiple tile layers for map 
+//         put after base layer code. ********
 
 // // Add GeoJSON data.
 // let sanFranAirport =
@@ -81,7 +85,7 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
     accessToken: API_KEY
 });
 
-// Skill Drill 13.4.1 Dark Tile Layer
+// Skill Drill 13.4.1 Dark Tile Layer (on 13.5.4 this will become another map view option)
 let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -102,10 +106,29 @@ let outdoors = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11
     accessToken: API_KEY
 });
 
-// Then we add our 'graymap' tile layer to the map
+// // Then we add our 'graymap' tile layer to the map
+// // ******** The code below is if you are only using one type of map view option*******
 // dark.addTo(map);
-streets.addTo(map);
+// streets.addTo(map);
 // night.addTo(map);
+
+// // Code below is what you use when you have multiple tile layers, which give you multiple map view options
+// Create a base layer that holds both maps.
+let baseMaps = {
+    Street: streets,
+    Dark: dark
+};
+
+// ******* Create the map object with alternative method, which is used when using multiple tile layers for map ********
+// Create the map object with a center and zoom level
+let map = L.map('mapid', {
+    center: [30, 30],
+    zoom: 2,
+    layers: [streets]
+});
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
 let airportData = "https://raw.githubusercontent.com/EmilyManchego-Stillwell/Mapping_Earthquakes/Mapping_GeoJSON_Points/Mapping_GeoJSON_Points/static/data/majorAirports.json";
